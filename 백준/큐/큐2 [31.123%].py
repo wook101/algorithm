477bcb74ed5c67cd8e1af0874521593dfddaf1c6
wook101 <ehddnr1022@gmail.com>
2021-04-22 23:17:52 +0900

    큐2

---
+++ 백준/큐/큐2 [31.123%].py
@@ -0,0 +1,34 @@
+
+from collections import deque
+import sys
+n=int(sys.stdin.readline())
+q = deque()
+
+for _ in range(n):
+    cmd = sys.stdin.readline().split()
+    if cmd[0]=="push":
+        q.append(cmd[1])
+    elif cmd[0]=="pop":
+        if q:
+            print(q.popleft())
+        else:
+            print(-1)
+    elif cmd[0]=="front":
+        if q:
+            print(q[0])
+        else:
+            print(-1)
+    elif cmd[0]=="size":
+        print(len(q))
+    elif cmd[0]=="back":
+        if q:
+            print(q[-1])
+        else:
+            print(-1)
+    elif cmd[0]=="empty":
+        if q:
+            print(0)
+        else:
+            print(1)
+
+
