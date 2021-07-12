5f0576d6a67e0bd5ca9811a6d4ab30a078ad3df7
wook101 <ehddnr1022@gmail.com>
2021-07-12 16:41:12 +0900

    숫자 문자열과 영단어

--- 알고리즘/로또추첨.py
+++ 알고리즘/로또추첨.py
@@ -1,7 +1,7 @@
 import random
 def soltion():
     lotto = []
-    numbers = [i for i in range(1,46)]  #1~45개 숫자가 담긴 리스트
+    numbers = [i for i in range(1,46)]  #1~45의 숫자가 담긴 리스트
 
     random.shuffle(numbers)             #리스트 한번 섞어주고
     for _ in range(6):
