6e4d572efee2cce53ae0a6d600de14242398ef52
wook101 <ehddnr1022@gmail.com>
2021-08-02 15:21:32 +0900

    부족한 금액 계산하기

---
+++ 프로그래머스/부족한금액 계산하기/부족한 금액 계산하기.py
@@ -0,0 +1,9 @@
+#count횟수 만큼 price에 곱해서 전체 금액구함
+#전체 지불금액(total)이 현재 보유금액(money) 보다 많으면 차를 구한다.
+#그렇지 않은경우 0을 리턴
+def solution(price, money, count):
+    total = 0
+    for i in range(1,count+1):
+        total += price*i
+    result = total-money if total > money else 0
+    return result
