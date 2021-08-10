21c9e1a1d7836c17916e4e87c34388597bdebfff
wook101 <ehddnr1022@gmail.com>
2021-08-10 19:31:57 +0900

    상호평가 (위클리챌린지 2주차 문제)

---
+++ 프로그래머스/위클리챌린지/상호평가.py
@@ -0,0 +1,32 @@
+def getGrade(avg):
+    if avg >= 90:
+        grade = "A"
+    elif avg >= 80:
+        grade = "B"
+    elif avg >= 70:
+        grade = "C"
+    elif avg >= 50:
+        grade = "D"
+    else:
+        grade = "F"
+    return grade
+
+
+def solution(scores):
+    result = []
+    n = len(scores)
+    for i in range(n):
+        tmpSum, tmp = 0, []
+        for j in range(n):
+            tmpSum += scores[j][i]
+            tmp.append(scores[j][i])
+
+        cnt = tmp.count(scores[i][i])
+        if (min(tmp) == scores[i][i] and cnt == 1) or (max(tmp) == scores[i][i] and cnt == 1):
+            avg = (tmpSum - scores[i][i]) / (n - 1)
+        else:
+            avg = tmpSum / n
+
+        result.append(getGrade(avg))
+
+    return ''.join(result)
