9817ff9627919bdbc8d6b41423b7a9e6d737e1f3
wook101 <ehddnr1022@gmail.com>
2021-08-25 20:57:32 +0900

    직업군 추천하기

---
+++ 프로그래머스/위클리챌린지/직업군 추천하기.py
@@ -0,0 +1,26 @@
+def solution(table, languages, preference):
+    answer = ''
+    size = len(table)
+    data = [{} for _ in range(size)]
+    tmp = [[] for _ in range(size)]
+    for i in range(size):
+        languList = table[i].split()
+        score = size
+        tmp[i].append(languList[0])
+        for langu in languList[1:]:
+            data[i][langu] = score
+            score -= 1
+
+    for i in range(size):
+        totalScore = 0
+        for j in range(len(languages)):
+            key = languages[j]
+            if not key in data[i]: continue
+            preferScore = preference[j]
+            languagesScore = data[i][key]
+            totalScore += preferScore * languagesScore
+        tmp[i].append(totalScore)
+
+    result = sorted(tmp, key=lambda x: (-x[1], x[0]))
+
+    return result[0][0]
