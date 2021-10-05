01b85a8e1666b4421b67cebec4ec833d7e0bbf1e
wook101 <ehddnr1022@gmail.com>
2021-10-06 00:12:39 +0900

    숫자놀이 update

--- 백준/문자열/숫자놀이.py
+++ 백준/문자열/숫자놀이.py
@@ -1,12 +1,17 @@
-m,n = input().split()
+def solution():
+    m,n = input().split()
+    data = {"1":"one","2":"two","3":"three","4":"four","5":"five"
+            ,"6":"six","7":"seven","8":"eight","9":"nine","0":"zero"}
+    arr = []
+    result=""
+    for num in range(min(int(m),int(n)),max(int(m),int(n))+1):
+        st=""
+        for c in str(num): st+=data[c]
+        arr.append((st,num))
 
-data = {"1":"one","2":"two","3":"three","4":"four","5":"five"
-        ,"6":"six","7":"seven","8":"eight","9":"nine","0":"zero"}
-arr = []
-for num in range(min(int(m),int(n)),max(int(m),int(n))+1):
-    st=""
-    for c in str(num): st+=data[c]
-    arr.append((st,num))
-res=""
-arr = sorted(arr,key=lambda x:x[0])
-
+    arr = sorted(arr,key=lambda x:x[0])
+    for i in range(len(arr)):
+        result+=str(arr[i][1])
+        if i%10==9: result+="\n"
+        else: result+=" "
+    return result
