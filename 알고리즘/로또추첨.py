a52823ac2920f2dc5fa058fb7bbc65469198924c
김동욱 <dw.kim@fssuniverse.com>
2024-05-09 12:26:11 +0900

    로또추첨 자동 5천원

--- 알고리즘/로또추첨.py
+++ 알고리즘/로또추첨.py
@@ -1,14 +1,15 @@
 import random
-def soltion():
+def solution():
     lotto = []
-    numbers = [i for i in range(1,46)]  #1~45의 숫자가 담긴 리스트
+    numbers = [i for _ in range(1,46)]  #1~45의 숫자가 담긴 리스트
 
     random.shuffle(numbers)             #리스트 한번 섞어주고
     for _ in range(6):
         num = numbers.pop()             #뒤에서 부터 숫자 6개 뽑음
         lotto.append(num)
 
-    return lotto
+    return sorted(lotto)
 
-print(soltion()) #[15, 1, 26, 36, 38, 8]
-
+#로또 자동 5천원
+for i in range(5):
+    print(solution())
