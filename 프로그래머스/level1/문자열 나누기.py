544a294a3aa20ff1cb1ea5627fcabc0960f861ca
김동욱 <dw.kim@fssuniverse.com>
2024-08-12 19:31:19 +0900

    문자열 나누기

---
+++ 프로그래머스/level1/문자열 나누기.py
@@ -0,0 +1,25 @@
+def solution(s):
+    res = []
+    x = ""
+    tmp = ""
+    cnt = 0
+    for i in range(len(s)):
+        if x == "":
+            x = s[i]
+            tmp = s[i]
+            cnt += 1
+            continue
+        if x == s[i]:
+            cnt += 1
+        else:
+            cnt -= 1
+        tmp += s[i]
+
+        if cnt == 0:
+            res.append(tmp)
+            x = ""
+            tmp = ""
+    if len(tmp) > 0:
+        res.append(tmp)
+
+    return len(res)
